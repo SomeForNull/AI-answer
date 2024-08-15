790d324fb4393ae6496f977f6fbddd67bb1ccd9d
SomeForNull <1400866473@qq.com>
2024-08-15 11:51:29 +0800

    引入智谱ai模型 封装通用方法 设计systemPrompt 调用模型接口

---
+++ AI-answer-backend/src/main/java/com/yunshu/aianswer/manager/AiManager.java
@@ -0,0 +1,72 @@
+package com.yunshu.aianswer.manager;
+
+import com.zhipu.oapi.ClientV4;
+import com.zhipu.oapi.Constants;
+import com.zhipu.oapi.service.v4.model.ChatCompletionRequest;
+import com.zhipu.oapi.service.v4.model.ChatMessage;
+import com.zhipu.oapi.service.v4.model.ChatMessageRole;
+import com.zhipu.oapi.service.v4.model.ModelApiResponse;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**\
+ * 通用Ai调用能力
+ */
+public class AiManager {
+    @Resource
+    private ClientV4 clientV4;
+    // 较稳定的随机数
+    private static final float STABLE_TEMPERATURE = 0.05f;
+
+    // 不稳定的随机数
+    private static final float UNSTABLE_TEMPERATURE = 0.99f;
+    public String doRequest(List<ChatMessage> messages, Boolean stream, Float temperature) {
+        // 构造请求
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
+                .model(Constants.ModelChatGLM4)
+                .stream(stream)
+                .invokeMethod(Constants.invokeMethod)
+                .temperature(temperature)
+                .messages(messages)
+                .build();
+        ModelApiResponse invokeModelApiResp = clientV4.invokeModelApi(chatCompletionRequest);
+        ChatMessage result = invokeModelApiResp.getData().getChoices().get(0).getMessage();
+        return result.getContent().toString();
+    }
+    public String doRequest(String systemMessage, String userMessage, Boolean stream, Float temperature) {
+        // 构造请求
+        List<ChatMessage> messages = new ArrayList<>();
+        ChatMessage systemChatMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), systemMessage);
+        ChatMessage userChatMessage = new ChatMessage(ChatMessageRole.USER.value(), userMessage);
+        messages.add(systemChatMessage);
+        messages.add(userChatMessage);
+        return doRequest(messages, stream, temperature);
+    }
+    public String doSyncRequest(String systemMessage, String userMessage, Float temperature) {
+        return doRequest(systemMessage, userMessage, Boolean.FALSE, temperature);
+    }
+    /**
+     * 同步调用（答案较稳定）
+     *
+     * @param systemMessage
+     * @param userMessage
+     * @return
+     */
+    public String doSyncStableRequest(String systemMessage, String userMessage) {
+        return doSyncRequest(systemMessage, userMessage, STABLE_TEMPERATURE);
+    }
+
+    /**
+     * 同步调用（答案较随机）
+     *
+     * @param systemMessage
+     * @param userMessage
+     * @return
+     */
+    public String doSyncUnstableRequest(String systemMessage, String userMessage) {
+        return doSyncRequest(systemMessage, userMessage, UNSTABLE_TEMPERATURE);
+    }
+
+}
---
+++ AI-answer-backend/src/test/java/com/yunshu/aianswer/ZhiPuAiTest.java
@@ -0,0 +1,38 @@
+package com.yunshu.aianswer;
+
+import com.zhipu.oapi.ClientV4;
+import com.zhipu.oapi.Constants;
+import com.zhipu.oapi.service.v4.model.ChatCompletionRequest;
+import com.zhipu.oapi.service.v4.model.ChatMessage;
+import com.zhipu.oapi.service.v4.model.ChatMessageRole;
+import com.zhipu.oapi.service.v4.model.ModelApiResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class ZhiPuAiTest {
+    @Resource
+    private ClientV4 clientV4;
+    @Test
+    public void test(){
+        // 构造请求
+        List<ChatMessage> messages = new ArrayList<>();
+        ChatMessage chatMessage = new ChatMessage(ChatMessageRole.USER.value(), "作为一名营销专家，请为智谱开放平台创作一个吸引人的slogan");
+        messages.add(chatMessage);
+        String requestId = String.valueOf(System.currentTimeMillis());
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
+                .model(Constants.ModelChatGLM4)
+                .stream(Boolean.FALSE)
+                .invokeMethod(Constants.invokeMethod)
+                .messages(messages)
+                .requestId(requestId)
+                .build();
+        // 调用
+        ModelApiResponse invokeModelApiResp = clientV4.invokeModelApi(chatCompletionRequest);
+        System.out.println("model output:" + invokeModelApiResp.getData().getChoices().get(0));
+    }
+}
